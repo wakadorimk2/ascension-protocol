172e31ddda00f673b8e7f61c628c5e3d5d6f01fe
wakadorimk2 <wakadori.mk2@gmail.com>
2024-09-18 03:44:13 +0900

    プレイヤーキャラクターにモデルを反映するパッチを作成

--- ascension-protocol/PlayerCharacterReplace.cs
+++ ascension-protocol/PlayerCharacterReplace.cs
@@ -1,6 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DMT;
+using HarmonyLib;
+
+public class MyModAPI : IModApi
+{
+    public void InitMod(Mod _modInstance)
+    {
+        Debug.Log("Initializing Mod API");
+
+        // モデルを置き換える
+        var harmony = new Harmony("com.ascension-protocol.mod");
+        harmony.PatchAll();
+    }
+}
+
+
+[HarmonyPatch(typeof(Player))]  // Playerクラスにパッチを適用
+[HarmonyPatch("Start")]  // プレイヤーの開始処理にパッチを適用
+public class PlayerPatch
+{
+    // Prefixはメソッドの実行前に呼ばれます
+    public static void Prefix(Player __instance)
+    {
+        Debug.Log("Custom Player Model Loaded");
+        // ここにカスタムプレイヤーモデルのロード処理を追加
+        PlayerCharacterReplace playerCharacterReplace = new PlayerCharacterReplace();
+        playerCharacterReplace.Start();
+        Debug.Log("Custom Player Model Loaded");
+        playerCharacterReplace.SetupPlayerAnimations();
+        Debug.Log("Custom Player Model Animations Setup");
+    }
+}
 
 
 public class PlayerCharacterReplace : MonoBehaviour
